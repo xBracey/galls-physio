56afba8c296be8e0698ffe544752e521be6595ca
Thomas Brace <tom@192.168.1.182>
2021-08-16 22:17:47 +0100

    Fix config file strapi

--- app/config/server.js
+++ app/config/server.js
@@ -1,9 +1,18 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '4ea71792e43ab38d55c65a5e17051681'),
+module.exports = ({ env }) => {
+  let url = "http://localhost";
+
+  if (env("NODE_ENV", "development") === "production") {
+    url = "https://firstteamphysiotherapy.co.uk/api";
+  }
+
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    admin: {
+      auth: {
+        secret: env("ADMIN_JWT_SECRET", "4ea71792e43ab38d55c65a5e17051681"),
+      },
     },
-  },
-});
+    url: url,
+  };
+};
