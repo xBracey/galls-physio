553fe8591e52963cdd33e3394f30a2434522eb0f
Thomas Brace <tom@192.168.1.182>
2021-08-17 18:35:39 +0100

    Moree rewrite

--- app/config/server.js
+++ app/config/server.js
@@ -2,14 +2,20 @@ module.exports = ({ env }) => {
   let url = "http://localhost";
 
   if (env("NODE_ENV", "development") === "production") {
-    url = "/api";
+    url = "https://firstteamphysiotherapy.co.uk/api";
+  }
+
+  if (env("NODE_ENV", "development") === "production") {
+    adminUrl = "https://firstteamphysiotherapy.co.uk/admin";
   }
 
   return {
     host: env("HOST", "0.0.0.0"),
     port: env.int("PORT", 1337),
     url,
+    proxy: true,
     admin: {
+      url: adminUrl,
       auth: {
         secret: env("ADMIN_JWT_SECRET", "4ea71792e43ab38d55c65a5e17051681"),
       },
