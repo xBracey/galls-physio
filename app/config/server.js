2fbba35b1c77665bd9e54c011dfd9ad4ebbfb4e1
Thomas Brace <tom@192.168.1.182>
2021-08-17 19:02:30 +0100

    Revert all settings

--- app/config/server.js
+++ app/config/server.js
@@ -1,22 +1,9 @@
 module.exports = ({ env }) => {
-  let url = "http://localhost";
-  let adminUrl = "http://localhost/admin";
-
-  if (env("NODE_ENV", "development") === "production") {
-    url = "https://firstteamphysiotherapy.co.uk/strapi";
-  }
-
-  if (env("NODE_ENV", "development") === "production") {
-    adminUrl = "https://firstteamphysiotherapy.co.uk/strapi/admin";
-  }
-
   return {
     host: env("HOST", "0.0.0.0"),
     port: env.int("PORT", 1337),
-    url,
     proxy: true,
     admin: {
-      url: adminUrl,
       auth: {
         secret: env("ADMIN_JWT_SECRET", "4ea71792e43ab38d55c65a5e17051681"),
       },
